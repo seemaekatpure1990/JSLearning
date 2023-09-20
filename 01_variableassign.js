d8ed524242ecd3d36410a32261225cf5abe8d15e
seemaekatpure1990 <seema.ekatpure1990@gmail.com>
2023-09-20 21:03:08 +0530

    variableassign
    variableassign

---
+++ 01_variableassign.js
@@ -0,0 +1,33 @@
+
+
+var myFullName;
+console.log(myFullName);
+
+myFullName="Seema ekatpure"
+console.log(myFullName);
+myFullName="Seema Vishwajeet Ekatpure"
+console.log(myFullName);
+
+var myLove ="I love only JS";
+console.log(myLove);
+
+var firstName="Seema"
+console.log( "firstName :", firstName);
+
+var lastName="Ekatpure";
+console.log("lastName :",lastName);
+
+var age="31"
+console.log("age :",age);
+
+var address="Akluj,Solapur"
+console.log("address :",address);
+
+var pinCode="413108";
+console.log("pinCode :",pinCode);
+
+var gender="Female";
+console.log("gender :",gender);
+
+var isMarried="Mrried";
+console.log("isMarried :",isMarried);
