65918d30e60aa38558aecee3f810e27f47650169
seemaekatpure1990 <seema.ekatpure1990@gmail.com>
2023-09-20 21:02:35 +0530

    variable
    variable

---
+++ 01_variable.js
@@ -0,0 +1,32 @@
+var firstName; // variable declaration
+
+firstName = "Seema";  // Variable initialization
+
+console.log(firstName);
+
+firstName = "Mrs.Seema"; // Variable update
+
+console.log(firstName);
+
+ 
+
+ 
+
+var lastName = "Ekatpure"; // variable declaration and initialization
+
+console.log(lastName);
+
+ 
+
+var height; // Variable Declaration
+
+console.log(height);
+
+height = "6"; // initialization
+
+console.log(height);
+
+height = "6.2 feet";// update
+
+console.log(height);
+
