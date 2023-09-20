9db353f15f88d8780077defb229423b970d4810d
seemaekatpure1990 <seema.ekatpure1990@gmail.com>
2023-09-20 21:30:30 +0530

    swap variable
    swap variable

---
+++ 01_swapVariable.js
@@ -0,0 +1,13 @@
+
+
+//WAP to swap variable values using third variable
+var num1="100";
+var num2="200";
+console.log("=========Before swap========");
+console.log(num1,num2);
+var temp=num1;
+num1=num2;
+num2=temp;
+console.log("=========After swap========");
+
+console.log(num1,num2);
