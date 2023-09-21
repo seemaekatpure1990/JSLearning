ddd18fba3921b10e20b5b8ceb0bc4287d9baadde
seemaekatpure1990 <seema.ekatpure1990@gmail.com>
2023-09-21 23:33:07 +0530

    variable
    variable

---
+++ 02_typeOf.js
@@ -0,0 +1,39 @@
+
+
+console.log("=====================Assignment 2===================");
+console.log("---------------step 1-----------------");
+console.log("-----Before initialization-----");
+var bankName;
+console.log("Bank Name :",bankName,"data type :",typeof bankName);
+
+console.log("-----After initialization-----");
+
+bankName="SBI Bank";
+console.log("Bank Name :",bankName,"       data type :",typeof bankName);
+
+
+console.log("---------------step 2-----------------");
+
+var marks='90%';
+console.log("Marks :",marks,"       data type :",typeof marks);
+
+console.log("---------------step 3-----------------");
+console.log("-----Before initialization-----");
+
+var isWorking;
+console.log("is Working :",isWorking, "      data type :",typeof isWorking);
+
+console.log("-----After initialization-----");
+
+isWorking=true;
+console.log("is Working :",isWorking,"      data type :",typeof isWorking);
+
+console.log("---------------step 3-----------------");
+
+var totalCount="Hundred and Seven";
+console.log("Total Count :",totalCount,"      data type :",typeof totalCount);
+
+console.log("----Updated-----");
+totalCount=107;
+console.log("total Count :",totalCount,"       data type :",typeof totalCount);
+
---
+++ 02_variableBasicAssig.js
@@ -0,0 +1,24 @@
+
+console.log("============Assignment 1=============");
+console.log("--------stringType----------");
+var firstName="Seema";
+console.log("first Name :",firstName,"type :" ,typeof firstName);
+
+console.log("---------numberType----------");
+var pin=413108;
+console.log("PIN :",pin,"type :",typeof pin);
+
+console.log("-----------booleanType--------");
+var isMarried = true;
+console.log("Married :",isMarried,"type :", typeof isMarried,);
+
+console.log("================NULL VS UNDEFINED================");
+var value;
+console.log("value of A :",value,"type :",typeof value);
+console.log("when variable is declared and not assigned any value then it is called Undefined ");
+
+value =null;
+console.log("value of B :",value,"type :",typeof value);
+console.log("Null value means there is no value but not equal to 0");
+
+
