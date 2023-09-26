d7efb4fb32338fa61d27f6cd9cd4ed8fa8fd6336
seemaekatpure1990 <seema.ekatpure1990@gmail.com>
2023-09-26 20:45:26 +0530

    string Assignment
    string Assignment

--- 03_stringHandsOn.js
+++ 03_stringHandsOn.js
@@ -6,6 +6,7 @@ function stringHandsOn() {
     var givenStr="   Hey you are doing good, keep it up    ";
     console.log(`1.Given string is ${givenStr}`);
     console.log(`2.string length is ${givenStr.length}`);
+    
     var beforeTrim=givenStr.length;
     var result=givenStr.trim();
     console.log( `3. Given string after trim is  ==> ${result}`);
@@ -14,18 +15,23 @@ function stringHandsOn() {
     var afterTrim=result.length;
     var trimResult = beforeTrim-afterTrim;
     console.log(`4.Total number of spaces removed  is ${trimResult}`);
+
     var charAt0=result.charAt(0);
     var lastIndexChar=result.charAt(result.length -1);
     console.log(`5. first char after trim is == ${charAt0}  || Last char after trim is == ${lastIndexChar}`);
+
     var words=result.split(" ");
     console.log(`6.Total word in string is == ${words.length}`);
+
     var indexOfGood=givenStr.indexOf('good');
     console.log(`7.Index of word good is ==${indexOfGood}`);
+
     var result1=givenStr.substring(26);
     console.log(`8.Substring starting from index 26 is ==${result1}`);
-    var result=givenStr.trim();
+    
     var endWith =result.endsWith('up');
     console.log(`9.String ends with word 'up' ==${endWith}`);
+
     var startWith=result.startsWith('Hey');
     console.log(`10.String starts with word 'Hey' == ${startWith}`);
 
