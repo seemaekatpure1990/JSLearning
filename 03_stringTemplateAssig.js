c73a159863d30374e5c3b4af9a083fb5a9e4a57b
seemaekatpure1990 <seema.ekatpure1990@gmail.com>
2023-09-26 20:40:39 +0530

    string Assignment
    string Assignment

---
+++ 03_stringTemplateAssig.js
@@ -0,0 +1,19 @@
+console.log(`========Assignment 1=========`);
+console.log(`-----step 1-----`);
+function word() {
+    console.log(`My dream company name is "Google"`);
+      
+}
+word();
+
+console.log(`-----step 2-----`);
+ 
+function stringTemplateAssign() {
+    var hobby1="Reading";
+    var hobby2="Drawing";
+    var hobby3="Programming";
+    console.log(`My hobbies are==> ||${hobby1}||  ||${hobby2}||  ||${hobby3}||`);
+    var result=hobby1.concat(  hobby2 ,  hobby3);
+    console.log(`After concating hobbies ==>  ${hobby1} ${hobby2} ${hobby3}`);
+}
+stringTemplateAssign();
