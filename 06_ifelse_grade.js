cfc63b36971025a5eaa06d49d9c492d6fe5765dc
seemaekatpure1990 <seema.ekatpure1990@gmail.com>
2023-09-29 19:57:32 +0530

    ifelaseA
    ifelaseA

---
+++ 06_ifelse_grade.js
@@ -0,0 +1,25 @@
+
+var voteEligible =function(age){
+     var result=+age;
+     if(age==0 || age<0 || age>=130 || age ==null || isNaN(result)) {
+        console.log(`Invalid data :${age}`);
+     } else {
+        if (age<18) {
+            console.log(`You are not eligible for vote. age :${age}`);
+
+        } else {
+            console.log(`You are eligible for vote. age :${age}`);
+
+        }
+        
+     }
+}
+voteEligible(45);
+voteEligible(17);
+voteEligible(8);
+voteEligible(20);
+voteEligible(-10);
+voteEligible(200);
+voteEligible(0);
+voteEligible(undefined);
+voteEligible(null);
