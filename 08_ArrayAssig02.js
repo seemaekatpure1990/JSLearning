6786afb42caab2199de5a417fc8c7cdea8f0ae90
seemaekatpure1990 <seema.ekatpure1990@gmail.com>
2023-10-08 21:43:20 +0530

    ArrayA
    ArrayA

---
+++ 08_ArrayAssig02.js
@@ -0,0 +1,28 @@
+
+const arrayNumbers=[20,31,40,25,23,11,29,9,60,2,11];
+console.log(`================================= 1 ======================================`);
+console.log(`Given Array is : [20,31,40,25,23,11,29,9,60,2,11]`);
+console.log(`Length of Array is : ${arrayNumbers.length}`); 
+
+console.log(`================================= 2 ======================================`);
+console.log(`First element in array is : ${arrayNumbers[0]}`);
+console.log(`Last element in array is : ${arrayNumbers[arrayNumbers.length-1]}`);
+
+console.log(`================================= 3 ======================================`);
+console.log(` 3 rd Last element in array is : ${arrayNumbers[arrayNumbers.length-3]}`);
+
+console.log(`================================= 4 ======================================`);
+console.log(`Given Array is : [20,31,40,25,23,11,29,9,60,2,11]`);
+for (const element in arrayNumbers) {
+    if (arrayNumbers.hasOwnProperty.call(arrayNumbers, element)) {
+        const element = object[element];
+        
+    }
+}
+
+
+
+
+
+    
+}
