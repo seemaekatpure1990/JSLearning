861811bf1b30716af4347687b3a21a662f2a09ec
seemaekatpure1990 <seema.ekatpure1990@gmail.com>
2023-10-11 21:45:55 +0530

    date
    date

---
+++ 09_date.js
@@ -0,0 +1,25 @@
+
+const date = new Date();
+console.log(date);
+
+const square10 = Math.pow(10,2);
+console.log(square10);
+
+const maxNum=Math.max(23.56, 67, 89, 90.567, 45, 67);
+console.log(maxNum);
+
+console.log(date.getDate());
+console.log(date.toDateString());
+console.log(date.toTimeString());
+
+var dt = new Date("Aug 28,2008 23:30:00");
+console.log(dt);
+
+const d = new Date("2015-03-25");
+console.log(d);
+
+const dd = new Date("03/25/2016");
+console.log(dd);
+
+const ddd = new Date("25 mar 2015");
+console.log(ddd);
